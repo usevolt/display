0d1a948e9ce4f1a478a567be15be8379a3c1ce37
olli.usenius <olli.usenius@usevolt.fi>
2016-10-24 10:51:11 +0300

    Project changed to use Usevolt makefile

--- sw/release/hal/uv_hal/src/ui/uwindow.d
+++
@@ -1,5 +0,0 @@
-hal/uv_hal/src/ui/uwindow.o hal/uv_hal/src/ui/uwindow.d: \
- ../hal/uv_hal/src/ui/uwindow.c \
- /home/usevolt/uw/display/sw/hal/uv_hal/inc/ui/uwindow.h
-
-/home/usevolt/uw/display/sw/hal/uv_hal/inc/ui/uwindow.h:
